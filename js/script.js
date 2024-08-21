44d9c0834f6722498ada4182a8fae53be88c5b1e
Lais Andrade <laisalves201988@gmail.com>
2024-08-21 16:08:53 -0300

    update final head

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const toggleTheme = document.getElementById('toggleTheme');
+const toggleTheme = document.getElementById("toggleTheme");
 
 const rootHtml = document.documentElement;
 
 
-// Função para alternar o Tema
+//função para alternar o tema 
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    currentTheme === "dark"? rootHtml.setAttribute ("data-theme", "light") : rootHtml.setAttribute ("data-theme", "dark");
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
-    toggleTheme.classList.toggle ("bi-sun");
-    toggleTheme.classList.toggle ("bi-moon-stars");
-}
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
+} 
 
 toggleTheme.addEventListener("click", changeTheme);
-// Fim da função mudar o tema
-
+//fim da função mudar o tema
